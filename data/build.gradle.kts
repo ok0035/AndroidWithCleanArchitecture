dc2a0745aa8bdaeedf0f1a0a561e274189ede311
zerosword <zerodeg93@gmail.com>
2024-03-23 14:24:19 +0900

    ZERO-8 Data Module 추가

---
+++ data/build.gradle.kts
@@ -0,0 +1,59 @@
+import Libs.androidTestImplementations
+import Libs.implementations
+import Libs.testImplementations
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.zerosword.data"
+    compileSdk = AppConfig.compileSdkVer
+
+    defaultConfig {
+        minSdk = AppConfig.minSdkVer
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = AppConfig.jvmTargetVer
+    }
+}
+
+dependencies {
+
+    val impls = listOf(
+        Libs.coreKtx,
+        Libs.material3
+    )
+
+    val testImpls = listOf(
+        Libs.junit
+    )
+
+    val androidTestImpls = listOf(
+        Libs.androidxTestJunit,
+        Libs.androidxEspressoCore
+    )
+
+    implementations(impls)
+    testImplementations(testImpls)
+    androidTestImplementations(androidTestImpls)
+
+}
