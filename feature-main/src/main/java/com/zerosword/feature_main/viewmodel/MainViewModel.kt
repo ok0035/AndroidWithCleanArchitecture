a7d6f6d62263d3cd818ff45923e3a80d79fafd74
zerosword <zerodeg93@gmail.com>
2024-04-08 13:12:08 +0900

    ZERO-21 FakeRepo가 남아있어 빌드가 되지 않던 이슈 수정

--- feature-main/src/main/java/com/zerosword/feature_main/viewmodel/MainViewModel.kt
+++ feature-main/src/main/java/com/zerosword/feature_main/viewmodel/MainViewModel.kt
@@ -1,11 +1,7 @@
 package com.zerosword.feature_main.viewmodel
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.zerosword.data.repositoryimpl.FakeMainRepositoryImpl
 import com.zerosword.domain.reporitory.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
